fa9ee50fa74d17e2bf9cc81aa6268497f6d66df8
Adalberto Pérez <ing.adalbertoperez@gmail.com>
2026-08-18 15:53:06 -0500

    Bump version to 1.1.1

--- webnova-core.php
+++ webnova-core.php
@@ -3,7 +3,7 @@
  * Plugin Name: WebNova Starter Kit
  * Plugin URI: https://agenciawebnova.com
  * Description: Importador de plantillas prediseñadas para sitios creados por Agencia WebNova.
- * Version: 1.0.0
+ * Version: 1.1.1
  * Author: Agencia WebNova
  * Text Domain: webnova-starter-kit
  * Requires at least: 6.5
@@ -16,7 +16,7 @@ if (! defined('ABSPATH')) {
     exit;
 }
 
-define('WEBNOVA_CORE_VERSION', '1.0.0');
+define('WEBNOVA_CORE_VERSION', '1.1.1');
 define('WEBNOVA_CORE_FILE', __FILE__);
 define('WEBNOVA_CORE_PATH', plugin_dir_path(__FILE__));
 define('WEBNOVA_CORE_URL', plugin_dir_url(__FILE__));
